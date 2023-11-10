b201367b5f480f015fe05a7342fec3e8bee209be
Santosh Philip <santosh@noemail.com>
2023-11-10 08:23:49 -0800

    modified:   .github/workflows/main.yml     modified:   tests/test_eppyread.py

--- tests/test_eppyread.py
+++ tests/test_eppyread.py
@@ -26,7 +26,7 @@ def test_getversion(iddname, idfname, expected):
     "iddname, idfname, expected",
     [
 #     (iddfhandle, StringIO("  Version,23.2.0;"), "23.2.0"), # iddname, idfname, expected
-    (iddfhandle, StringIO("  Version,8.2.0;"), "8.2.0"), # iddname, idfname, expected
+    (iddfhandle, StringIO("  Version,8.3.0;"), "8.3.0"), # iddname, idfname, expected
     ]
 )
 def test_getversion_1(iddname, idfname, expected):
